bdc1c7bb520cc49aaf214ab4f4db195d8e457969
maksiz32 <maks-manzulin@mail.ru>
2021-05-31 03:17:04 +0300

    End of less2: SHOP & HAMBURGERS

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ class GoodsItem {
     }
     render() {
         return `<div class="goods-item">
-                <img class="goods-item-pic" src="img/${this.img} "width="120" height="100">
-                <h3 class="goods-item-title">${this.title}</h3>
-                <p class="goods-item-price">${this.price}</p>
-                <button type="button" class="goods-item-btn">Добавить</button>
-            </div>`;
+                    <img class="goods-item-pic" src="img/${this.img}" alt="${this.img}" width="120" height="100">
+                    <h3 class="goods-item-title">${this.title}</h3>
+                    <p class="goods-item-price">${this.price}</p>
+                    <button type="button" class="goods-item-btn">Добавить</button>
+                </div>`;
     }
 }
 
@@ -34,8 +34,49 @@ class GoodsList {
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
     }
+    sumAllGoods() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        })
+        const totalPrice = `<div class="goods-total">Итого: ${sum} руб</div>`;
+        document.querySelector('.goods-list').insertAdjacentHTML('afterend', totalPrice);
+    }
+}
+
+class Basket extends GoodsList {
+    fetchGoods(prod) {
+        //
+    }
+    render() {
+        //
+    }
+    totalPrice() {
+        //
+    }
+}
+
+class ItemInBasket extends GoodsItem {
+    constructor() {
+        this.count = 1;
+    }
+    plusCount() {
+        //
+    }
+    minusCount() {
+        //
+    }
+    sumPriceItems() {
+        //
+    }
+    render() {
+        return `<div class="basket-item">
+                    <span>${this.title}</span><span>${this.count}</span><span>${this.price}</span>
+                </div>`;
+    }
 }
 
 const list = new GoodsList();
 list.fetchGoods();
 list.render();
+console.log(list.sumAllGoods());
